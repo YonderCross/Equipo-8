a8398bf9c6fb9030b89178218b1bc881d0036ae3
carolina orea <dream.stephorea@ciencias.unam.mx>
2022-04-29 00:31:41 -0500

    update: actualizo formularios

--- form/formcontact.js
+++ form/formcontact.js
@@ -1,82 +1,24 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const pass = document.getElementById("contraseña")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	contraseña: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,10}$/ // 7 a 10 numeros.
-}
-
-const campos = {
-	
-	nombre: false,
-	correo: false,
-	telefono: false,
-    contraseña: false,
-}
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-		
-		case "contraseña":
-			validarCampo(expresiones.contraseña, e.target, 'contraseña');
-			validarccontraseña();
-		break;
-		case "ccontraseña":
-			validarccontraseña();
-		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
-		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
-		break;
-	}
-}
-
-
-
-const validarccontraseña = () => {
-	const inputcontraseña = document.getElementById('contraseña');
-	const inputccontraseña = document.getElementById('ccontraseña');
-
-	if(inputcontraseña.value !== inputccontraseña.value){
-		document.getElementById(`ccontraseña`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`ccontraseña`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(` i`).classList.add('fa-times-circle');
-		document.querySelector(`i`).classList.remove('fa-check-circle');
-		
-		campos['contraseña'] = false;
-	} else {
-		document.getElementById(`ccontraseña`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	
-
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings =" "
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    console.log(regexEmail.test(correo.value))
+    if(!regexEmail.test(correo.value)){
+        warnings += `El correo no es valido <br>`
+        entrar = true
+    
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
