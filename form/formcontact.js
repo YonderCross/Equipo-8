3293dd8de64a39e946c791baa88d5fb29a97ec94
carolina orea <dream.stephorea@ciencias.unam.mx>
2022-05-04 12:08:12 -0500

    update: actualice las validaciones de contraseña y telefono

--- form/formcontact.js
+++ form/formcontact.js
@@ -1,6 +1,7 @@
 const nombre = document.getElementById("nombre")
 const correo = document.getElementById("correo")
 const pass = document.getElementById("contraseña")
+const passc = document.getElementById("ccontraseña")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 
@@ -10,15 +11,27 @@ form.addEventListener("submit", e=>{
     let entrar = false
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
     parrafo.innerHTML = ""
+    
     console.log(regexEmail.test(correo.value))
     if(!regexEmail.test(correo.value)){
         warnings += `El correo no es valido <br>`
         entrar = true
     
     }
+    if(pass.value != passc.value){
+        warnings += `La contraseña debe ser la misma <br>`
+    }
     if(entrar){
         parrafo.innerHTML = warnings
-    }else{
+    }
+  
+    else{
         parrafo.innerHTML = "Enviado"
     }
+})
+
+var input=  document.getElementById('numero');
+input.addEventListener('input',function(){
+  if (this.value.length > 10) 
+     this.value = this.value.slice(0,10); 
 })
